07971947f419ffc2f35cf8c0348c7dd08022feca
Jeno Mario Varga <baphomet14@gmail.com>
2023-10-20 20:01:33 +0200

    feat: add logout link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,16 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Navigation() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  const userIsLoggedIn = !!token;
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
+
   return (
     <nav>
       <ul>
@@ -8,7 +18,11 @@ function Navigation() {
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/register">Register</Link></li>
-        <li><Link to="/login">Login</Link></li>
+        {userIsLoggedIn ? (
+          <li><Link to="/login" onClick={logout}>Logout</Link></li>
+        ) : (
+          <li><Link to="/login">Login</Link></li>
+        )}
         <li><Link to="/dashboard">Dashboard</Link></li>
       </ul>
     </nav>
